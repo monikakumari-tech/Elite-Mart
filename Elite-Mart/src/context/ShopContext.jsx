4a91c9ae0e5613610f3cdca736a37e5630d7c884
Monika18092023 <mk.18092023@gmail.com>
2025-03-12 23:04:12 +0530

    contextAPI Logic

--- Elite-Mart/src/context/ShopContext.jsx
+++ Elite-Mart/src/context/ShopContext.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { createContext } from "react";
+import { products } from "../assets/assets";
 
-const ShopContext = () => {
-  return (
-    <div>ShopContext</div>
-  )
+// creating context API
+export const ShopContext=createContext();
+
+const ShopContextProvider=(props)=>{
+    const currency='$';
+    const delivery_fee=10;
+
+
+    const value={
+         currency,
+         delivery_fee,
+         products,
+
+    }
+    return(
+        <ShopContext.Provider value={value}>
+          {props.children}
+        </ShopContext.Provider>
+
+    )
 }
-export default ShopContext
+
+export default ShopContextProvider;
