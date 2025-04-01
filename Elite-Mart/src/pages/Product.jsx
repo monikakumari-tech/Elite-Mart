d918eafbd5094875d6436b909aec318c75068d51
Monika18092023 <mk.18092023@gmail.com>
2025-04-01 23:22:56 +0530

    product logic

--- Elite-Mart/src/pages/Product.jsx
+++ Elite-Mart/src/pages/Product.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const Product = () => {
-  return (
-    <div>Product</div>
-  )
-}
-export default Product
+  const [productdata, setProductdata] = useState(false);
+  const { products } = useContext(ShopContext);
+  const { productId } = useParams();
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      return item._id == productId ? setProductdata(item) : null;
+    });
+  };
+  useEffect(() => {
+    fetchProductData();
+  }, [productId]);
+
+  console.log(productdata);
+  return productdata ? <div>product</div> : <div></div>;
+};
+export default Product;
