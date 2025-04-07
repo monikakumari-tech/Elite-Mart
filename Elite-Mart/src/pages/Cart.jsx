0377806a051cd19aa24655f4825580aae64d936c
Monika18092023 <mk.18092023@gmail.com>
2025-04-07 23:22:14 +0530

    cart UI ready

--- Elite-Mart/src/pages/Cart.jsx
+++ Elite-Mart/src/pages/Cart.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title';
+import { assets } from '../assets/assets';
 
 const Cart = () => {
+  const {cartItem,products,currency}=useContext(ShopContext);
+  console.log(cartItem);
+  const [cartData, setCartData]= useState([])
+  useEffect(()=>{
+   const tempData=[]
+   for(const items in cartItem){
+     for(const item in cartItem[items]){
+      if(cartItem[items][item]>0){
+         tempData.push({
+          _id:items,
+          size:item,
+          quantity:cartItem[items][item]
+         })
+      }
+     }
+   }
+   setCartData(tempData)
+  },[cartItem])
+ 
   return (
-    <div>Cart</div>
+    <>
+   <Title text1={"Your"} text2={"Cart"}/>
+   <hr/>
+       {cartData.map((item, index)=>{
+        const productData= products.find((product)=>product._id==item._id)
+        return (
+          <>
+          <div key={index}>
+            <img src={productData.image[0]} alt="image" className=''/>
+            <div>
+            <p>{productData.name}</p>
+            <p><span>{currency}</span>{productData.price}</p>
+            <p>{item.size}</p>
+            </div>
+          </div>
+          <div>
+            {item.quantity}
+          </div>
+          <div>
+            <img src={assets.bin_icon} alt="delete" />
+          </div>
+          </>
+        )
+       })}
+   </>
   )
 }
 export default Cart
