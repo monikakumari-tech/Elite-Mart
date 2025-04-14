5b5e92e087fdc3e6793e578c3e4b5240132585e4
Monika18092023 <mk.18092023@gmail.com>
2025-04-14 23:04:26 +0530

    userController endpoint api

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,12 @@
+
+const loginUser= async (req, res)=>{
+
+}
+const registerUser= async (req, res)=>{
+  res.send({message:"register api is working"})
+}
+const adminLogin= async (req, res)=>{
+  
+}
+
+export {loginUser, registerUser, adminLogin}
