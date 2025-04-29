a5727f4063f7779b2b1e85ec85434cb78bc4371f
Monika18092023 <mk.18092023@gmail.com>
2025-04-29 23:12:01 +0530

    login and registeration logic

--- Elite-Mart/src/pages/Home.jsx
+++ Elite-Mart/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { assets } from "../assets/assets";
+
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
 import BestSeller from "../components/BestSeller";
