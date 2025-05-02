7239dcb215e2f5235efef392188384662da2e9be
Monika18092023 <mk.18092023@gmail.com>
2025-05-02 22:46:46 +0530

    cart API

--- Elite-Mart/src/context/ShopContext.jsx
+++ Elite-Mart/src/context/ShopContext.jsx
@@ -1,135 +1,170 @@
 import { createContext, useEffect, useState } from "react";
 // import { products } from "../assets/assets";
 import { toast } from "react-toastify";
-import axios from "axios"
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 // creating context API
-export const ShopContext=createContext();
+export const ShopContext = createContext();
 
-const ShopContextProvider=(props)=>{
-  const backend_url= import.meta.env.VITE_BACKEND_URL
-  const [token, setToken] = useState("")
-  const navigate= useNavigate()
-  const [products, setProducts]= useState([])
+const ShopContextProvider = (props) => {
+  const backend_url = import.meta.env.VITE_BACKEND_URL;
+  const [token, setToken] = useState("");
+  const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
 
-    const currency='$';
-    const delivery_fee=10;
-  const [search, setSearch]= useState("")
-  const [showSearch, setShowSearch]= useState(false)
-  const [cartItem, setCartItem]= useState({})
+  const currency = "$";
+  const delivery_fee = 10;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(false);
+  const [cartItem, setCartItem] = useState({});
 
-  const addToCart= async (itemId, size)=>{
-    if(!size){
-      toast.error("please select size")
-      return 
+  const addToCart = async (itemId, size) => {
+    if (!size) {
+      toast.error("please select size");
+      return;
     }
-     let cartData= structuredClone(cartItem)   //structured clone make deep copy of cartItem
-     console.log(cartData)
-     if(cartData[itemId]){                  //  if cartData[id] this key exist then
-      if(cartData[itemId][size]){
-        cartData[itemId][size]+=1
-      }else{
-        cartData[itemId][size]=1
+    let cartData = structuredClone(cartItem); //structured clone make deep copy of cartItem
+    console.log(cartData);
+    if (cartData[itemId]) {
+      //  if cartData[id] this key exist then
+      if (cartData[itemId][size]) {
+        cartData[itemId][size] += 1;
+      } else {
+        cartData[itemId][size] = 1;
       }
-     }else{
-       cartData[itemId]={}           // here it making nested object
-       cartData[itemId][size]=1     // e.g.- { aa:{S:1} }
-       
-     }
-     setCartItem(cartData)
-  }
-  const getCartCount=()=>{
-    let totalCount= 0;
-    for(const items in cartItem){
-      for(const item in cartItem[items]){
+    } else {
+      cartData[itemId] = {}; // here it making nested object
+      cartData[itemId][size] = 1; // e.g.- { aa:{S:1} }
+    }
+    setCartItem(cartData);
+    if (token) {
+      try {
+        await axios.post(
+          backend_url + "/api/cart/add",
+          { itemId, size },
+          { headers: { token } }
+        );
+      } catch (error) {
+        res.error.message;
+      }
+    }
+  };
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItem) {
+      for (const item in cartItem[items]) {
         try {
-          if(cartItem[items][item]>0){
-            totalCount+=cartItem[items][item]
+          if (cartItem[items][item] > 0) {
+            totalCount += cartItem[items][item];
           }
         } catch (error) {
-          console.log(error)
+          console.log(error);
         }
       }
     }
-    return totalCount
-  }
-  const updateQuantity = async (itemId, size, quantity)=>{
-    let cartData= structuredClone(cartItem)
-    cartData[itemId][size]=quantity
-    setCartItem(cartData)
-  }
+    return totalCount;
+  };
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItem);
+    cartData[itemId][size] = quantity;
+    setCartItem(cartData);
 
-  const getCartTotal = ()=>{
-    let totalAmount=0;
-    for(const items in cartItem){
-      let itemInfo= products.find((product)=>product._id==items)
-     for(const item in cartItem[items]){
+    if (token) {
       try {
-        if(cartItem[items][item]>0){
-          totalAmount+=itemInfo.price*cartItem[items][item]
-          
-        }
+        await axios.post(
+          backend_url + "/api/cart/update",
+          { itemId, size, quantity },
+          { headers: { token } }
+        );
       } catch (error) {
-        console.log(error)
+        res.error.message;
       }
-     }
     }
-    return totalAmount
-  }
+  };
 
-  useEffect(()=>{
-    
-    console.log(cartItem)
-   
-  },[cartItem])
-  const getProductData= async ()=>{
-    try{
-      const response = await axios.get(backend_url+"/api/product/list")
-      console.log(response.data)
-      if(response.data.success){
-        setProducts(response.data.products)
-      }else{
-        toast.error(response.data.message)
+  const getCartTotal = () => {
+    let totalAmount = 0;
+    for (const items in cartItem) {
+      let itemInfo = products.find((product) => product._id == items);
+      for (const item in cartItem[items]) {
+        try {
+          if (cartItem[items][item] > 0) {
+            totalAmount += itemInfo.price * cartItem[items][item];
+          }
+        } catch (error) {
+          console.log(error);
+        }
       }
-    }catch(error){
-      toast.error(error.message)
-      console.log(error)
     }
-  }
-  useEffect(()=>{
-   getProductData()
-  },[])
-  useEffect(()=>{
-    if(!token && localStorage.getItem("token")){
-      setToken(localStorage.getItem("token"))
-    }
-  },[])
-    const value={
-       backend_url,
-       token,
-       setToken,
-       navigate,
-         currency,
-         delivery_fee,
-         products,
-         search,
-         setSearch, 
-         showSearch,
-         setShowSearch,
-         cartItem,
-         setCartItem,
-         addToCart, 
-         getCartCount,
-         updateQuantity,
-         getCartTotal
-    }
-    return(
-        <ShopContext.Provider value={value}>
-          {props.children}
-        </ShopContext.Provider>
+    return totalAmount;
+  };
 
-    )
-}
+  useEffect(() => {
+    console.log(cartItem);
+  }, [cartItem]);
+  const getProductData = async () => {
+    try {
+      const response = await axios.get(backend_url + "/api/product/list");
+      console.log(response.data);
+      if (response.data.success) {
+        setProducts(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+      console.log(error);
+    }
+  };
+
+  const getUserCart = async (token) => {
+    try {
+      const response = await axios.post(
+        backend_url + "/api/cart/get",
+        {},
+        { headers: { token } }
+      );
+      console.log(response.data, "line 125");
+      if (response.data.success) {
+        setCartItem(response.data.cartData);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+  useEffect(() => {
+    getProductData();
+  }, []);
+
+  useEffect(() => {
+    if (!token && localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+      getUserCart(localStorage.getItem("token"));
+    }
+  }, []);
+  const value = {
+    backend_url,
+    token,
+    setToken,
+    navigate,
+    currency,
+    delivery_fee,
+    products,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+    cartItem,
+    setCartItem,
+    addToCart,
+    getCartCount,
+    updateQuantity,
+    getCartTotal,
+  };
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
--- Elite-Mart/src/pages/Cart.jsx
+++ Elite-Mart/src/pages/Cart.jsx
@@ -1,96 +1,115 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
-import Title from '../components/Title';
-import { assets } from '../assets/assets';
-import CartTotal from '../components/CartTotal';
-import { Link } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
+import CartTotal from "../components/CartTotal";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-  const {cartItem,products,currency,updateQuantity}=useContext(ShopContext);
-  const [totalPrice, setTotalPrice]= useState([])
-  
+  const { cartItem, products, currency, updateQuantity } =
+    useContext(ShopContext);
+  const [totalPrice, setTotalPrice] = useState([]);
+
   console.log(cartItem);
-  const [cartData, setCartData]= useState([])
-  useEffect(()=>{
-   const tempData=[]
-   for(const items in cartItem){
-     for(const item in cartItem[items]){
-      if(cartItem[items][item]>0){
-         tempData.push({
-          _id:items,
-          size:item,
-          quantity:cartItem[items][item]
-         })
-  
+  const [cartData, setCartData] = useState([]);
+  useEffect(() => {
+    if(products.length>0){
+const tempData = [];
+    for (const items in cartItem) {
+      for (const item in cartItem[items]) {
+        if (cartItem[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItem[items][item],
+          });
+        }
+        setCartData(tempData);
       }
-      setCartData(tempData)
+    }
     
-     }
-   }
-   
-  },[cartItem ])
- 
+    }
+  }, [cartItem,products]);
+
   return (
     <>
-    <hr />
-    <div className='pt-17 pb-3'>
-    <Title text1={"Your"} text2={"Cart"}/>
-    </div>
-  
-   <hr/>
-       {cartData.map((item, index)=>{
-        const productData= products.find((product)=>product._id==item._id)
-        
-        
+      <hr />
+      <div className="pt-17 pb-3">
+        <Title text1={"Your"} text2={"Cart"} />
+      </div>
+
+      <hr />
+      {cartData.map((item, index) => {
+        const productData = products.find((product) => product._id == item._id);
+
         return (
           <>
-          <div key={index} className='flex py-5 justify-between items-center'>
-          <div className='flex gap-4'>
-            <img src={productData.image[0]} alt="image" className='w-25 sm:w-30'/>
-            <div className='flex flex-col text-xl text-gray-700  py-7'>
-            <p>{productData.name}</p>
-            <div className='flex gap-4 items-center'>
-            <p><span>{currency}</span>{productData.price}</p>
-            <p className='border py-1 px-3'>{item.size}</p>
+            <div key={index} className="flex py-5 justify-between items-center">
+              <div className="flex gap-4">
+                <img
+                  src={productData.image[0]}
+                  alt="image"
+                  className="w-25 sm:w-30"
+                />
+                <div className="flex flex-col text-xl text-gray-700  py-7">
+                  <p>{productData.name}</p>
+                  <div className="flex gap-4 items-center">
+                    <p>
+                      <span>{currency}</span>
+                      {productData.price}
+                    </p>
+                    <p className="border py-1 px-3">{item.size}</p>
+                  </div>
+                </div>
+              </div>
+
+              <input
+                type="number"
+                defaultValue={item.quantity}
+                min={1}
+                className=" border py-3 px-2 "
+                onChange={(e) =>
+                  e.target.value === "" || e.target.value === "0"
+                    ? null
+                    : updateQuantity(
+                        item._id,
+                        item.size,
+                        Number(e.target.value)
+                      )
+                }
+              />
+
+              <div>
+                <img
+                  src={assets.bin_icon}
+                  alt="delete"
+                  className="w-6"
+                  onClick={() => updateQuantity(item._id, item.size, 0)}
+                />
+              </div>
             </div>
-            
-            </div>
-          </div>
-           
-              <input type="number" defaultValue={item.quantity} min={1} className=' border py-3 px-2 ' onChange={(e)=>e.target.value==="" || e.target.value==="0"? null:updateQuantity(item._id, item.size,Number(e.target.value))}/>
-            
-         
-            <div>
-            <img src={assets.bin_icon} alt="delete" className='w-6' onClick={()=>updateQuantity(item._id, item.size, 0)} />
-          </div>
-          </div>
-          
-          
-          <hr/>
-          
-          
+
+            <hr />
           </>
-        )
-       })}
-       <div className='flex justify-end 
-       my-20'>
-        <div className='w-full sm:w-[450px]'>
-       <CartTotal/>
-
+        );
+      })}
+      <div
+        className="flex justify-end 
+       my-20"
+      >
+        <div className="w-full sm:w-[450px]">
+          <CartTotal />
         </div>
-       </div>
+      </div>
 
-<div className="flex justify-end">
-        <Link to='/place-order' >
-          <button className="bg-black text-white py-4 px-8 mt-2 text-right" >
+      <div className="flex justify-end">
+        <Link to="/place-order">
+          <button className="bg-black text-white py-4 px-8 mt-2 text-right">
             Proceed To CheckOut
           </button>
-          </Link>
-        </div>
-         
-        
-
-   </>
-  )
-}
-export default Cart
+        </Link>
+      </div>
+    </>
+  );
+};
+export default Cart;
