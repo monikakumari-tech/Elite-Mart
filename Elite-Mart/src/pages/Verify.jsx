9359f3dc6000fd3d5105496f508dd4d31e4e225f
Monika18092023 <mk.18092023@gmail.com>
2025-05-10 22:58:39 +0530

    stripe payment verify

--- Elite-Mart/src/pages/Verify.jsx
+++ Elite-Mart/src/pages/Verify.jsx
@@ -1,6 +1,39 @@
+import { useContext, useEffect } from "react"
+import { ShopContext } from "../context/ShopContext"
+import { useSearchParams } from "react-router-dom"
+import axios from "axios"
+
 const Verify = () => {
-  return (
-    <div>Verify</div>
+  const {navigate, token , backend_url, setCartItem}= useContext(ShopContext)
+  
+  const [searchParam, setSearchParam]= useSearchParams()
+
+  const success= searchParam.get("success")
+  const orderId= searchParam.get("orderId")
+
+  const verify= async ()=>{
+
+    try {
+      if(!token){
+        return null
+      }
+      const response= await axios.post(backend_url + "/api/order/verify", {success,orderId}, {headers:{token}} )
+      if(response.data.success){
+        setCartItem({})
+        navigate("/orders")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+   verify()
+  },[token])
+  console.log(success)
+    return (
+    <div>
+
+    </div>
   )
 }
 export default Verify
